6b5f8fe015a595d8f3e70b90ee0ce542be923a97
OzodN <ytnuritdinov@gmail.com>
2026-04-04 19:19:56 +0500

    refactored

--- src/server/Main.server.lua
+++ src/server/Main.server.lua
@@ -8,7 +8,7 @@ local services = Loader.LoadFolder(ServicesFolder)
 -- init order
 services.DataService:Init()
 
-for name, service in pairs(services) do
+for _, service in pairs(services) do
 	if service.Init then
 		service:Init(services)
 	end
